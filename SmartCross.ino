baaf1c8f3ce7d8516eabf915fb1fb732bcb990d5
Mateus <sonicheats@gmail.com>
2026-04-16 17:09:49 -0300

    Add distance calculation function and trigger pins

--- SmartCross.ino
+++ SmartCross.ino
@@ -10,7 +10,12 @@ int tempoVerde = 5000;
 int tempoPedestre = tempoVerde;
 int tempoEspera = 1000; 
 
+const int pinTrigger = 6;
+const int pinEccho = 7;
+
+float CalcularDistancia();
 void EstadoSemaforo(int estado);
+void GerenciarTravessia(int tempoBaseTravessia);
 
 void setup()
 {
@@ -30,6 +35,25 @@ estado++;
   EstadoSemaforo(estado);
   if(estado > 4) estado = 1;
 
+}
+
+float CalcularDistancia()
+{
+  digitalWrite(pinTrigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pinTrigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pinTrigger, LOW);
+
+  // Lê o tempo de retorno do eco
+  long duracao = pulseIn(pinEccho, HIGH);
+
+  // Calcula a Distância do obstáculo até o sensor: D = v * t
+  // onde "v" é a velocidade do som e "t" é o tempo
+  float distancia = (0.034 * duracao) / 2;
+  // Divide por 2 pois o pulso vai e volta, percorre o mesmo espaço duas vezes
+
+  return distancia;
 }
 
 void EstadoSemaforo(int estado)
